1f277f85f629d09945e0d0e1edba0706b5d010e1
Kauã Davi <kauadaviamaro@gmail.com>
2025-03-21 01:21:33 -0300

    refactor: update email service to use environment variables and add .env.example file

--- instacar-api/src/services/email.service.ts
+++ instacar-api/src/services/email.service.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: process.env.EMAIL_SERVICE,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
